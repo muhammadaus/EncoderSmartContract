ea1d873c36fc0921fe53fd37ed8af398c5e8a7a2
MUHAMMAD AUS <aushijree@gmail.com>
2024-10-19 23:00:28 +0900

    add write functions to pass to another contract

--- contracts/Encoder.sol
+++ contracts/Encoder.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract UniversalEncoder {
+contract Encoder {
 
     // Function to encode a single uint256
     function encodeUint256(uint256 value) public pure returns (bytes memory) {
@@ -28,21 +28,6 @@ contract UniversalEncoder {
         return abi.encode(values);
     }
 
-    // Function to encode a struct
-    struct Person {
-        string name;
-        uint256 age;
-    }
-
-    function encodeStruct(Person memory person) public pure returns (bytes memory) {
-        return abi.encode(person);
-    }
-
-    // Function to encode a dynamic array of structs
-    function encodeDynamicArrayOfStructs(Person[] memory people) public pure returns (bytes memory) {
-        return abi.encode(people);
-    }
-
     // Function to encode a boolean
     function encodeBool(bool value) public pure returns (bytes memory) {
         return abi.encode(value);
@@ -56,5 +41,62 @@ contract UniversalEncoder {
     // Function to encode multiple types
     function encodeMultipleTypes(uint256 num, string memory text, bool flag, address addr) public pure returns (bytes memory) {
         return abi.encode(num, text, flag, addr);
+    }
+
+    // Function to encode data and pass it to another contract
+    function encodeAndPassData(
+        address targetContract,
+        bytes memory encodedData
+    ) internal {
+        (bool success, ) = targetContract.call(encodedData);
+        require(success, "Target contract call failed");
+    }
+
+    // Function to encode and pass uint256
+    function encodeAndPassUint256(address targetContract, uint256 value) public {
+        bytes memory encodedData = encodeUint256(value);
+        encodeAndPassData(targetContract, encodedData);
+    }
+
+    // Function to encode and pass string
+    function encodeAndPassString(address targetContract, string memory value) public {
+        bytes memory encodedData = encodeString(value);
+        encodeAndPassData(targetContract, encodedData);
+    }
+
+    // Function to encode and pass multiple parameters
+    function encodeAndPassMultiple(address targetContract, uint256 value, string memory text) public {
+        bytes memory encodedData = encodeMultiple(value, text);
+        encodeAndPassData(targetContract, encodedData);
+    }
+
+    // Function to encode and pass array of uint256
+    function encodeAndPassUint256Array(address targetContract, uint256[] memory values) public {
+        bytes memory encodedData = encodeUint256Array(values);
+        encodeAndPassData(targetContract, encodedData);
+    }
+
+    // Function to encode and pass array of strings
+    function encodeAndPassStringArray(address targetContract, string[] memory values) public {
+        bytes memory encodedData = encodeStringArray(values);
+        encodeAndPassData(targetContract, encodedData);
+    }
+
+    // Function to encode and pass boolean
+    function encodeAndPassBool(address targetContract, bool value) public {
+        bytes memory encodedData = encodeBool(value);
+        encodeAndPassData(targetContract, encodedData);
+    }
+
+    // Function to encode and pass address
+    function encodeAndPassAddress(address targetContract, address value) public {
+        bytes memory encodedData = encodeAddress(value);
+        encodeAndPassData(targetContract, encodedData);
+    }
+
+    // Function to encode and pass multiple types
+    function encodeAndPassMultipleTypes(address targetContract, uint256 num, string memory text, bool flag, address addr) public {
+        bytes memory encodedData = encodeMultipleTypes(num, text, flag, addr);
+        encodeAndPassData(targetContract, encodedData);
     }
 }
